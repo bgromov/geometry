61374b6a8b9714197678b0722eab8aff242b0dc4
0xff <0xff.root@gmail.com>
2013-08-01 22:46:31 +0900

    Experimenting with dynamic limits change

--- tf/src/static_transform_publisher.cpp
+++ tf/src/static_transform_publisher.cpp
@@ -37,15 +37,16 @@ class TransformSender
 public:
   ros::NodeHandle node_;
   //constructor
-  TransformSender(double x, double y, double z, double yaw, double pitch, double roll, ros::Time time, const std::string& frame_id, const std::string& child_frame_id)
-  { 
+  TransformSender(double x, double y, double z, double yaw, double pitch, double roll, ros::Time time, const std::string& frame_id, const std::string& child_frame_id) :
+    angle_units_(0)
+  {
     tf::Quaternion q;
     q.setRPY(roll, pitch,yaw);
     transform_ = tf::StampedTransform(tf::Transform(q, tf::Vector3(x,y,z)), time, frame_id, child_frame_id );
     reconf_init();
   };
   TransformSender(double x, double y, double z, double qx, double qy, double qz, double qw, ros::Time time, const std::string& frame_id, const std::string& child_frame_id) :
-    transform_(tf::Transform(tf::Quaternion(qx,qy,qz,qw), tf::Vector3(x,y,z)), time, frame_id, child_frame_id)
+    angle_units_(0), transform_(tf::Transform(tf::Quaternion(qx,qy,qz,qw), tf::Vector3(x,y,z)), time, frame_id, child_frame_id)
   {
     reconf_init();
   };
@@ -63,42 +64,194 @@ public:
     broadcaster.sendTransform(transform_);
   };
 
-  //
-  void reconf_callback(tf::TransformSenderConfig &config, uint32_t level)
+  // Switch limits
+  void reconf_rpy_limits()
   {
-    if(level == 0xffffffff) // sent by dynamic reconfigure at first run
+    tf::TransformSenderConfig min_conf, max_conf;
+
+    // Get current limits
+    reconf_server_.getConfigMin(min_conf);
+    reconf_server_.getConfigMax(max_conf);
+
+    // Update only RPY limits
+    if(angle_units_ == USE_RADIANS)
     {
-      double R, P, Y;
-
-      // Update config with current values
-      config.x = transform_.getOrigin().x();
-      config.y = transform_.getOrigin().y();
-      config.z = transform_.getOrigin().z();
-
-      transform_.getBasis().getRPY(R, P, Y);
-      config.roll = R;
-      config.pitch = P;
-      config.yaw = Y;
+      // Radians
+      min_conf.roll = min_conf.pitch = min_conf.yaw = -M_PI;
+      max_conf.roll = max_conf.pitch = max_conf.yaw = M_PI;
     }
     else
     {
-      tf::Quaternion q;
-      tf::Transform t;
+      // Degrees
+      min_conf.roll = min_conf.pitch = min_conf.yaw = -180.0;
+      max_conf.roll = max_conf.pitch = max_conf.yaw = 180.0;
+    }
 
-      q.setRPY(config.roll, config.pitch, config.yaw);
-      t = tf::Transform(q, tf::Vector3(config.x, config.y, config.z));
-      transform_ = tf::StampedTransform(t, ros::Time::now(), transform_.frame_id_, transform_.child_frame_id_);
+    // Set new limits
+    reconf_server_.setConfigMin(min_conf);
+    reconf_server_.setConfigMax(max_conf);
+  }
+
+  // Dynamic reconfigure callback
+  void reconf_callback(tf::TransformSenderConfig &config, uint32_t level)
+  {
+    tf::Quaternion q;
+    tf::Transform t;
+    double R, P, Y;
+
+    ROS_INFO_STREAM("Level: " << level);
+
+    switch(level)
+    {
+      // Sent by dynamic reconfigure at first run
+      case CHANGE_ALL:
+          transform_.getBasis().getRPY(R, P, Y);
+          // Radians to degrees
+          if(angle_units_ == USE_DEGREES) toDegrees(R, P, Y);
+
+          // Update config with current values
+          config.x = transform_.getOrigin().x();
+          config.y = transform_.getOrigin().y();
+          config.z = transform_.getOrigin().z();
+
+          config.roll = R;
+          config.pitch = P;
+          config.yaw = Y;
+
+          config.qw = transform_.getRotation().w();
+          config.qx = transform_.getRotation().x();
+          config.qy = transform_.getRotation().y();
+          config.qz = transform_.getRotation().z();
+          break;
+
+      case CHANGE_XYZ:
+        // Update translation only
+        t = tf::Transform(transform_.getRotation(), tf::Vector3(config.x, config.y, config.z));
+        transform_ = tf::StampedTransform(t, ros::Time::now(), transform_.frame_id_, transform_.child_frame_id_);
+        break;
+
+      case CHANGE_RPY:
+        R = config.roll; P = config.pitch; Y = config.yaw;
+
+        // Degrees to radians
+        if(angle_units_ == USE_DEGREES) toRadians(R, P, Y);
+
+        q.setRPY(R, P, Y);
+
+        // Update orientation only
+        t = tf::Transform(q, transform_.getOrigin());
+        transform_ = tf::StampedTransform(t, ros::Time::now(), transform_.frame_id_, transform_.child_frame_id_);
+
+        // Update quaternion
+        config.qw = transform_.getRotation().w();
+        config.qx = transform_.getRotation().x();
+        config.qy = transform_.getRotation().y();
+        config.qz = transform_.getRotation().z();
+        break;
+
+      case CHANGE_QUAT:
+        q = tf::Quaternion(config.qx, config.qy, config.qz, config.qw);
+
+        // If new quaternion is not valid use previous one and issue error
+        if(q.length2() == 0.0){
+          q = transform_.getRotation();
+          ROS_ERROR("Reconfigure: quaternion length cannot be 0.0. Using previous value");
+        }
+        // Check normalization
+        else if(!q.normalized())
+        {
+          q = q.normalize();
+          ROS_WARN("Reconfigure: quaternion is not normilized. Normalizing.");
+        }
+
+        // Update orientation only
+        t = tf::Transform(q, transform_.getOrigin());
+        transform_ = tf::StampedTransform(t, ros::Time::now(), transform_.frame_id_, transform_.child_frame_id_);
+
+        // Update quaternion with corrected value
+        config.qw = q.w();
+        config.qx = q.x();
+        config.qy = q.y();
+        config.qz = q.z();
+
+        // Update RPY
+        transform_.getBasis().getRPY(R, P, Y);
+        // Radians to degrees
+        if(angle_units_ == USE_DEGREES) toDegrees(R, P, Y);
+
+        config.roll = R;
+        config.pitch = P;
+        config.yaw = Y;
+
+        // Reset checkbox
+        config.use_quaternion = false;
+        break;
+
+      case CHANGE_UNITS:
+        // if not changed then do nothing
+        if (angle_units_ == config.angle_units) break;
+
+        angle_units_ = config.angle_units;
+
+        ROS_INFO_STREAM("UNITS: " << angle_units_);
+
+        reconf_rpy_limits();
+
+        // Update RPY
+        transform_.getBasis().getRPY(R, P, Y);
+
+        // Radians to degrees
+        if(angle_units_ == USE_DEGREES)
+        {
+          toDegrees(R, P, Y);
+          config.angle_units = angle_units_;
+        }
+
+        config.roll = R;
+        config.pitch = P;
+        config.yaw = Y;
+
+        break;
     }
   }
 
 private:
+  enum {
+    CHANGE_NOTHING = 0,
+    CHANGE_XYZ = 1 << 0,
+    CHANGE_RPY = 1 << 1,
+    CHANGE_QUAT = 1 << 2,
+    CHANGE_UNITS = 1 << 3,
+    CHANGE_ALL = 0xffffffff
+  };
+
+  enum {
+    USE_RADIANS = 0,
+    USE_DEGREES = 1
+  };
+
   tf::StampedTransform transform_;
   dynamic_reconfigure::Server<tf::TransformSenderConfig> reconf_server_;
+  int angle_units_;
 
   // Set dynamic reconfigure callback
   void reconf_init()
   {
     reconf_server_.setCallback(boost::bind(&TransformSender::reconf_callback, this, _1, _2));
+  }
+
+  void toDegrees(double &r, double &p, double &y)
+  {
+    r = r * 180.0 / M_PI;
+    p = p * 180.0 / M_PI;
+    y = y * 180.0 / M_PI;
+  }
+
+  void toRadians(double &r, double &p, double &y)
+  {
+    r = r / 180.0 * M_PI;
+    p = p / 180.0 * M_PI;
+    y = y / 180.0 * M_PI;
   }
 };
 
